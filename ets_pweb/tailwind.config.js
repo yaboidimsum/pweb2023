2d3a42111fe86c819f22290303b3e81fbc2fc29c
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-10-19 15:49:20 +0700

    feat: add ets pweb

---
+++ ets_pweb/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./job.html"],
+  theme: {
+    extend: {
+      screens: {
+        "2xl": "2160px",
+      },
+    },
+    plugins: [],
+  },
+};
