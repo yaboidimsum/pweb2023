c4c2d9cca8172a26f3063d656a67bd46529e1f11
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-10-19 20:59:24 +0700

    feat new page

--- ets_pweb/tailwind.config.js
+++ ets_pweb/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./job.html"],
+  content: ["./index.html", "./job.html","./page.html"],
   theme: {
     extend: {
       screens: {
